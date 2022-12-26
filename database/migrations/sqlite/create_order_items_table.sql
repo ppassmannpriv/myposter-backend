71cf18b6e43d9f3757cb56d51ea2ce817ec2a273
Pieter Passmann <pieter.passmann@azerion.com>
2022-12-27 00:16:06 +0100

    Changed SQLite DB

--- database/migrations/sqlite/create_order_items_table.sql
+++ database/migrations/sqlite/create_order_items_table.sql
@@ -3,11 +3,12 @@ CREATE TABLE order_items
     id integer
         constraint order_items_pk
             primary key autoincrement,
-    item_type varchar(50),
+    item_category varchar(50) default 'production' not null,
+    item_type varchar(50) not null,
     amount integer default 1 not null,
-    image varchar(255) not null,
-    size_height integer not null,
-    size_width integer not null,
+    image varchar(255) null,
+    size_height integer null,
+    size_width integer null,
     order_id integer not null
         constraint order_items_orders_null_fk
             references orders (id)
--- database/migrations/sqlite/insert_testing_data.sql
+++ database/migrations/sqlite/insert_testing_data.sql
@@ -1,36 +1,17 @@
-INSERT INTO `orders`
-VALUES (
-        1000001,
-        (SELECT `id`, `delivery_address_id`, `invoice_address_id` FROM `customers` WHERE `firstname` = 'Bruce' AND `lastname` = 'Wayne'),
-        'ordered',
-        '2021-01-01 00:00:00'
-    )
+INSERT INTO `customers` (`id`, `firstname`, `lastname`)
+VALUES (1000001, 'Bruce', 'Wayne'),
+       (1000002, 'Peter', 'Parker');
 
-VALUES (1000001, 'Bruce Wayne', 'Bruce Wayne, 1007 Mountain Drive, Gotham', 'Bruce Wayne, 1007 Mountain Drive, Gotham',
-        '{\"items\":[{\"item_type\":\"poster\",\"size_height\":80,\"size_width\":60,\"amount\":2,\"image\":\"/files/poster_1.jpg\"},{\"item_type\":\"poster\",\"size_height\":80,\"size_width\":60,\"amount\":1,\"image\":\"/files/poster_2.jpg\"}]}',
-        '2021-01-01 00:00:00', 'ordered'),
-       (1000001, 'Bruce Wayne', 'Bruce Wayne, 1007 Mountain Drive, Gotham', 'Bruce Wayne, 1007 Mountain Drive, Gotham',
-        '{\"items\":[{\"item_type\":\"canvas\",\"size_height\":80,\"size_width\":80,\"amount\":1,\"image\":\"/files/canvas_1.jpg\"}]}',
-        '2021-01-01 00:00:00', 'shipped'),
-       (1000002, 'Peter Parker', 'Maybelle Parker, 15th Street, Queens, New York City, New York',
-        'Peter Parker, 20 Ingram Street, Forest Hills, Queens, New York City, New York',
-        '{\"items\":[{\"item_type\":\"canvas\",\"size_height\":80,\"size_width\":80,\"amount\":1,\"image\":\"/files/canvas_1.jpg\"},{\"item_type\":\"poster\",\"size_height\":20,\"size_width\":20,\"amount\":2,\"image\":\"/files/poster_123.jpg\"},{\"item_type\":\"canvas\",\"size_height\":80,\"size_width\":80,\"amount\":3,\"image\":\"/files/canvas_2.jpg\"},{\"item_category\":\"accessory\",\"item_type\":\"tape\",\"amount\":4},{\"item_category\":\"frame\",\"item_type\":\"wooden-frame-1\",\"size_height\":80,\"size_width\":80,\"amount\":3},{\"item_category\":\"frame\",\"item_type\":\"wooden-frame-2\",\"size_height\":20,\"size_width\":20,\"amount\":2}]}',
-        '2021-01-01 00:00:00', 'slicing');
-
-INSERT INTO `customers` ('firstname', 'lastname')
-VALUES ('Bruce', 'Wayne'), ('Peter', 'Parker');
-
-INSERT INTO `addresses` ('firstname', 'lastname', 'street', 'house_number', 'zipcode', 'city', 'customer_id')
+INSERT INTO `addresses` (`firstname`, `lastname`, `street`, `house_number`, `zipcode`, `city`, `customer_id`)
 VALUES ('Bruce', 'Wayne', 'Mountain Drive', '1007', null, 'Gotham', (
         SELECT `id` FROM `customers` WHERE `firstname` = 'Bruce' AND `lastname` = 'Wayne'
-    )
-), ('Maybelle', 'Parker', '15th Street', null, null, 'Queens, New York City, New York', (
+    )),
+    ('Maybelle', 'Parker', '15th Street', null, null, 'Queens, New York City, New York', (
         SELECT `id` FROM `customers` WHERE `firstname` = 'Peter' AND `lastname` = 'Parker'
-    )
-), ('Peter', 'Parker', 'Ingram Street', '20', null, 'Forest Hills, Queens, New York City, New York', (
-    SELECT `id` FROM `customers` WHERE `firstname` = 'Peter' AND `lastname` = 'Parker'
-)
-)
+    )),
+    ('Peter', 'Parker', 'Ingram Street', '20', null, 'Forest Hills, Queens, New York City, New York', (
+        SELECT `id` FROM `customers` WHERE `firstname` = 'Peter' AND `lastname` = 'Parker'
+    ))
 
 UPDATE `customers` SET
     `invoice_address_id` = (SELECT `id` FROM `addresses` WHERE `firstname` = 'Bruce' AND `lastname` = 'Wayne'),
@@ -41,3 +22,35 @@ UPDATE `customers` SET
     `invoice_address_id` = (SELECT `id` FROM `addresses` WHERE `firstname` = 'Peter' AND `lastname` = 'Parker'),
     `delivery_address_id` = (SELECT `id` FROM `addresses` WHERE `firstname` = 'Maybelle' AND `lastname` = 'Parker')
 WHERE `firstname` = 'Peter' AND `lastname` = 'Parker';
+
+INSERT INTO `orders` (`customer_id`, `delivery_address_id`, `invoice_address_id`, `status`, `created_at`)
+VALUES (1000001,
+        (SELECT `delivery_address_id` FROM `customers` WHERE `id` = 1000001),
+        (SELECT `invoice_address_id` FROM `customers` WHERE `id` = 1000001),
+        'ordered',
+        '2021-01-01 00:00:00'),
+        (1000001,
+        (SELECT `delivery_address_id` FROM `customers` WHERE `id` = 1000001),
+        (SELECT `invoice_address_id` FROM `customers` WHERE `id` = 1000001),
+        'shipped',
+        '2021-01-01 00:00:00'),
+        (1000002,
+        (SELECT `delivery_address_id` FROM `customers` WHERE `id` = 1000002),
+        (SELECT `invoice_address_id` FROM `customers` WHERE `id` = 1000002),
+        'slicing',
+        '2021-01-01 00:00:00');
+
+INSERT INTO `order_items` (`item_type`, `amount`, `image`, `size_height`, `size_width`, `order_id`)
+VALUES  ('poster', 2, '/files/poster_1.jpg', 80, 60, (SELECT `id` FROM `orders` WHERE `customer_id` = 1000001 AND `status` = 'ordered')),
+        ('poster', 1, '/files/poster_2.jpg', 80, 60, (SELECT `id` FROM `orders` WHERE `customer_id` = 1000001 AND `status` = 'ordered')),
+        ('canvas', 1, '/files/canvas_1.jpg', 80, 80, (SELECT `id` FROM `orders` WHERE `customer_id` = 1000001 AND `status` = 'shipped')),
+        ('canvas', 1, '/files/canvas_1.jpg', 80, 80, (SELECT `id` FROM `orders` WHERE `customer_id` = 1000002 AND `status` = 'slicing')),
+        ('poster', 2, '/files/poster_123.jpg', 20, 20, (SELECT `id` FROM `orders` WHERE `customer_id` = 1000002 AND `status` = 'slicing')),
+        ('canvas', 3, '/files/canvas_2.jpg', 80, 80, (SELECT `id` FROM `orders` WHERE `customer_id` = 1000002 AND `status` = 'slicing'));
+
+INSERT INTO `order_items` (`item_type`, `amount`, `item_category`, `order_id`)
+VALUES  ('tape', 4, 'accessory', (SELECT `id` FROM `orders` WHERE `customer_id` = 1000002 AND `status` = 'slicing'));
+
+INSERT INTO `order_items` (`item_type`, `amount`, `item_category`, `size_height`, `size_width`, `order_id`)
+VALUES  ('wooden-frame-1', 3, 'frame', 80, 80, (SELECT `id` FROM `orders` WHERE `customer_id` = 1000002 AND `status` = 'slicing')),
+        ('wooden-frame-2', 2, 'frame', 20, 20, (SELECT `id` FROM `orders` WHERE `customer_id` = 1000002 AND `status` = 'slicing'));
